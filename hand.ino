90a7b14bee186a42f2f2c2c24e581f68e5a50f4a
LianMollick <78555845+MevrickNeal@users.noreply.github.com>
2026-01-03 04:28:00 +0600

    Update hand.ino

--- hand.ino
+++ hand.ino
@@ -3,35 +3,34 @@
 
 Adafruit_PWMServoDriver pwm = Adafruit_PWMServoDriver();
 
-int channels[] = {2, 3, 4, 5, 6}; 
-int openVals[] = {450, 450, 450, 590, 450};
-int closeVals[] = {60, 60, 60, 60, 60};
-
 void setup() {
   Serial.begin(115200);
   pwm.begin();
   pwm.setPWMFreq(50);
-  relaxAll();
 }
 
 void relaxAll() {
-  for (int i = 0; i < 5; i++) pwm.setPin(channels[i], 4096, true); 
-}
-
-void moveHand(bool open) {
-  for (int i = 0; i < 5; i++) {
-    pwm.setPWM(channels[i], 0, open ? openVals[i] : closeVals[i]);
-  }
+  for (int i = 2; i <= 6; i++) pwm.setPin(i, 4096, true); 
 }
 
 void loop() {
   if (Serial.available() > 0) {
-    char cmd = Serial.read();
-    if (cmd == 'A') moveHand(true);
-    if (cmd == 'B') moveHand(false);
-    if (cmd == 'C') { // Middle Finger Gesture
-      for(int i=0; i<5; i++) pwm.setPWM(channels[i], 0, (channels[i] == 5) ? 590 : 60);
+    char head = Serial.read();
+    
+    // PRECISION MOVEMENT
+    if (head == 'C') {
+      int chan = Serial.parseInt();
+      if (Serial.read() == ',') {
+        int pulse = Serial.parseInt();
+        // Constrain for safety
+        if (pulse < 60) pulse = 60;
+        if (pulse > 600) pulse = 600;
+        pwm.setPWM(chan, 0, pulse);
+      }
+    } 
+    // EMERGENCY RELAX
+    else if (head == 'R') {
+      relaxAll();
     }
-    if (cmd == 'R') relaxAll(); // RELAX COMMAND
   }
 }
